8bcf5608e094cbaf9f7ff651a80f64275394c4a3
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2023-05-21 19:08:39 +0200

    result

--- src/components/CornerVector.jsx
+++ src/components/CornerVector.jsx
@@ -7,34 +7,70 @@ import { useState, useEffect } from "react";
 export const CornerVector = React.memo(({type, left, right}) => {
 
     const [windowSize, setWindowSize] = useState(false);
+    const [small, setSmall] = useState(false);
 
     useEffect(() => {
         let size = window.matchMedia("(max-width: 1710px)");
         if(size.matches) setWindowSize(true);
     }, []);
 
+    useEffect(() => {
+        let size = window.matchMedia("(max-width: 668px)");
+        if(size.matches) setSmall(true);
+    }, []);
+
     if(type === 'left') {
         return (
-            <motion.img 
-            src={!windowSize ? left : "https://i.ibb.co/RhsJvHK/Vector-2.png"} 
-            alt="Left Corner Background"
-            className={styles.LeftCornerBackground}
-            variants={cornerLeftVariant}
-            initial={'hidden'}
-            animate={'visible'}
+            <>
+                {
+                    small
+                    ?
+                    <motion.img 
+                    src="https://i.ibb.co/gyDCRt7/Vector-4.png" 
+                    alt="Left Corner Background"
+                    className={styles.LeftCornerBackground}
+                    variants={cornerLeftVariant}
+                    initial={'hidden'}
+                    animate={'visible'}
+                    />
+                    :
+                    <motion.img 
+                    src={!windowSize ? left : "https://i.ibb.co/RhsJvHK/Vector-2.png"} 
+                    alt="Left Corner Background"
+                    className={styles.LeftCornerBackground}
+                    variants={cornerLeftVariant}
+                    initial={'hidden'}
+                    animate={'visible'}
             />
+                }
+            </>
         )
     }
     else {
         return (
-            <motion.img 
-            src={!windowSize ? right : "https://i.ibb.co/jMrZ07g/Vector-3.png"}
-            alt="Right Corner Background"
-            className={styles.RightCornerBackground}
-            variants={cornerRightVariant}
-            initial={'hidden'}
-            animate={'visible'}
+            <>
+                {
+                    small
+                    ?
+                    <motion.img 
+                    src="https://i.ibb.co/ns6zJPG/Frame-2103.png"
+                    alt="Right Corner Background"
+                    className={styles.RightCornerBackground}
+                    variants={cornerRightVariant}
+                    initial={'hidden'}
+                    animate={'visible'}
+                    />
+                    :
+                    <motion.img 
+                    src={!windowSize ? right : "https://i.ibb.co/jMrZ07g/Vector-3.png"}
+                    alt="Right Corner Background"
+                    className={styles.RightCornerBackground}
+                    variants={cornerRightVariant}
+                    initial={'hidden'}
+                    animate={'visible'}
             />
+                }
+            </>
         )
     }
 })
